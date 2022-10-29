4166a214c505bc41347f8580595db55165ff3922
baishijiadeniao <2410287648@qq.com>
2022-10-29 11:00:17 +0800

    finish

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 #获取项目绝对路径
 current_path=$(cd "$(dirname $0)";pwd)
 
+if [ ! -d "${current_path}/build" ]; then
+    mkdir ${current_path}/build/
+fi
+
 cd ${current_path}/build
 rm -rf ./*
 cmake ..
